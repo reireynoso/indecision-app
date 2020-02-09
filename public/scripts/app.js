3bb76fa68343144a2e65c09284b9319114641009
reireynoso <reireynoso@gmail.com>
2020-02-08 22:24:13 -0500

    rendered 2nd template

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,10 +5,53 @@ console.log("App.js is running");
 // JSX Javascript syntax extension
 
 var template = React.createElement(
-  "p",
-  null,
-  "Does this hey App.js"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Does this hey App.js"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is Some info"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item One"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item two"
+        )
+    )
+);
+
+var template2 = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Rei Rey"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: 29"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: Jersey City"
+    )
 );
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
--- src/app.js
+++ src/app.js
@@ -2,7 +2,24 @@ console.log("App.js is running")
 
 // JSX Javascript syntax extension
 
-var template = <p>Does this hey App.js</p>;
+var template = (
+    <div>
+        <h1>Does this hey App.js</h1>
+        <p>This is Some info</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var template2 = (
+    <div>
+        <h1>Rei Rey</h1>
+        <p>Age: 29</p>
+        <p>Location: Jersey City</p>
+    </div>
+)
 var appRoot = document.getElementById("app")
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template2, appRoot)
