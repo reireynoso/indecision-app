d86a2aa73ffa2f4b9f53a21a13f3fd4a77b25909
reireynoso <reireynoso@gmail.com>
2020-02-09 18:45:03 -0500

    arguments/this bound to arrow

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,13 +1,12 @@
-"use strict";
-
 //arguments object - no longer bound with arrow functions
 
 
-var add = function add(a, b) {
+const add = function(a,b){
     // console.log(arguments)
-    return a + b;
-};
-console.log(add(55, 1));
+    return a + b
+
+}
+console.log(add(55,1))
 
 // const addArrow = (a,b) => {
 //     console.log(arguments)
@@ -18,7 +17,7 @@ console.log(add(55, 1));
 
 //this keyword - no longer bound with arrow functions
 
-var user = {
+const user = {
     name: "Rei",
     cities: ["Jersey", "NY"],
     // printPlacesLived: function(){
@@ -28,35 +27,26 @@ var user = {
     //         console.log(city)
     //     })
     // }
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        //method definition es6
+    printPlacesLived(){ //method definition es6
         // console.log(this.name)
         // console.log(this.cities)
-        return this.cities.map(function (city) {
-            return _this.name + " has lived in " + city;
-        });
+        return this.cities.map((city) => this.name + " has lived in " + city)
         // this.cities.forEach((city) => {
         //     console.log(this.name, city)
         // })
     }
-};
+}
 
-console.log(user.printPlacesLived());
+console.log(user.printPlacesLived())
 
 // When we use a regular function defined on an object property, the this keyword is bound to the object
 
-var multiplier = {
-    numbers: [1, 2, 3, 4, 5, 6],
+const multiplier = {
+    numbers: [1,2,3,4,5,6],
     multiplyBy: 2,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (num) {
-            return num * _this2.multiplyBy;
-        });
+    multiply(){
+        return this.numbers.map((num) => num * this.multiplyBy)
     }
-};
+}
 
-console.log(multiplier.multiply());
+console.log(multiplier.multiply())
