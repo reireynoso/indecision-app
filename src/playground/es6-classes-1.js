24f9b1a9c67551bd106ccefc96f68cf1f9b860c0
reireynoso <reireynoso@gmail.com>
2020-02-23 19:48:11 -0500

    es6 classes part 2

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -20,6 +20,43 @@ class Person {
     }
 }
 
-const me = new Person("Rei", 24);
+class Student extends Person {
+    constructor(name, age, major){
+        super(name, age)
+        this.major = major
+    }
 
-console.log(me.getDescription())
+    hasMajor(){
+        return !!this.major
+    }
+
+    getDescription(){
+        let description = super.getDescription();
+        if(this.hasMajor()){
+            description += ` Their major is ${this.major}`
+        }
+        return description
+    }
+}
+
+// Traveler -> Person
+// Add support for homeLocation
+
+class Traveler extends Person{
+    constructor(name, age, homeLocation){
+        super(name, age)
+        this.homeLocation = homeLocation
+    }
+
+    getGreeting(){
+        let greeting = super.getGreeting()
+        if(this.homeLocation){
+            greeting += `. I am visiting from Brooklyn`
+        }
+        return greeting
+    }
+}
+
+const me = new Traveler("Rei", 24, "Math");
+
+console.log(me.getGreeting())
