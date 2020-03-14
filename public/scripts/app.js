2474c4be66f596adea64bfe18256334f9d230d54
reireynoso <reireynoso@gmail.com>
2020-03-14 16:07:49 -0400

    setState with prevState argument

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -19,49 +19,69 @@ var Counter = function (_React$Component) {
         _this.handleAddOne = _this.handleAddOne.bind(_this);
         _this.handleMinusOne = _this.handleMinusOne.bind(_this);
         _this.handleReset = _this.handleReset.bind(_this);
+
+        _this.state = {
+            count: 0
+        };
         return _this;
     }
 
     _createClass(Counter, [{
-        key: "handleAddOne",
+        key: 'handleAddOne',
         value: function handleAddOne() {
-            console.log("clicked Add");
+            // console.log("clicked Add")    
+            this.setState(function (prevState) {
+                return {
+                    count: prevState.count + 1
+                };
+            });
         }
     }, {
-        key: "handleMinusOne",
+        key: 'handleMinusOne',
         value: function handleMinusOne() {
-            console.log("clicked Minus");
+            // console.log("clicked Minus")
+            this.setState(function (prevState) {
+                return {
+                    count: prevState.count - 1
+                };
+            });
         }
     }, {
-        key: "handleReset",
+        key: 'handleReset',
         value: function handleReset() {
-            console.log("clicked Reset");
+            // console.log("clicked Reset")
+            this.setState(function () {
+                return {
+                    count: 0
+                };
+            });
         }
     }, {
-        key: "render",
+        key: 'render',
         value: function render() {
             return React.createElement(
-                "div",
+                'div',
                 null,
                 React.createElement(
-                    "h1",
+                    'h1',
                     null,
-                    "Count: "
+                    'Count: ',
+                    this.state.count
                 ),
                 React.createElement(
-                    "button",
+                    'button',
                     { onClick: this.handleAddOne },
-                    "+ 1"
+                    '+ 1'
                 ),
                 React.createElement(
-                    "button",
+                    'button',
                     { onClick: this.handleMinusOne },
-                    "- 1"
+                    '- 1'
                 ),
                 React.createElement(
-                    "button",
+                    'button',
                     { onClick: this.handleReset },
-                    "Reset"
+                    'Reset'
                 )
             );
         }
--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -4,23 +4,42 @@ class Counter extends React.Component {
         this.handleAddOne = this.handleAddOne.bind(this);
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
+
+        this.state = {
+            count: 0
+        }
     }
     handleAddOne(){
-        console.log("clicked Add")        
+        // console.log("clicked Add")    
+        this.setState((prevState) => {
+            return {
+                count: prevState.count + 1
+            }
+        })    
     }
 
     handleMinusOne(){
-        console.log("clicked Minus")
+        // console.log("clicked Minus")
+        this.setState((prevState) => {
+            return {
+                count: prevState.count - 1
+            }
+        })   
     }
 
     handleReset(){
-        console.log("clicked Reset")
+        // console.log("clicked Reset")
+        this.setState(() => {
+            return {
+                count: 0
+            }
+        })   
     }
 
     render(){
         return(
         <div>
-            <h1>Count: </h1>
+            <h1>Count: {this.state.count}</h1>
             <button onClick={this.handleAddOne}>+ 1</button>
             <button onClick={this.handleMinusOne}>- 1</button>
             <button onClick={this.handleReset}>Reset</button>
