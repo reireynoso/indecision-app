366904310970169f867484c6642fc34eb8906f2e
reireynoso <reireynoso@gmail.com>
2020-03-17 12:11:45 -0400

    named exports

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log("new app!!")
+// // import './utils';
+// import {square, add} from './utils'
+// console.log("new app!!")
+// console.log(square(4))
+// console.log(add(2,3))
+
+import {isAdult, canDrink} from './person'
+
+console.log(isAdult(14))
+console.log(canDrink(21))
