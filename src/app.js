b93035924a165ea24f067fa53978bf9a28448b3e
reireynoso <reireynoso@gmail.com>
2020-03-17 14:19:58 -0400

    importing npm modules

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
+import validator from 'validator';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const template = React.createElement('p', {}, 'testing123')
+ReactDOM.render(template, document.getElementById('app'))
+
 // import './utils';
 // import subtract, {square, add} from './utils'
 // console.log("new app!!")
@@ -5,8 +12,12 @@
 // console.log(add(2,3))
 // console.log(subtract(3,2))
 
-import isSenior, {isAdult, canDrink} from './person'
+// import isSenior, {isAdult, canDrink} from './person'
 
-console.log(isAdult(14))
-console.log(canDrink(21))
-console.log(isSenior(64));
+// console.log(isAdult(14))
+// console.log(canDrink(21))
+// console.log(isSenior(64));
+
+// Importing NPM modules
+// Install -> import -> use
+// npm install validator
