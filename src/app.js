c89401badffd36d67956ec9ccbf7d272b31db6b3
reireynoso <reireynoso@gmail.com>
2020-03-17 20:16:41 -0400

    babel with webpack

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import validator from 'validator';
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const template = React.createElement('p', {}, 'testing123')
+const template = <p>This JSX webpack</p>
 ReactDOM.render(template, document.getElementById('app'))
 
 // import './utils';
