1c2b727e9dbad33a575c3ac8e3e278e876b38be1
reireynoso <reireynoso@gmail.com>
2020-03-19 19:46:33 -0400

    passing children to componet

--- src/app.js
+++ src/app.js
@@ -3,8 +3,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import IndecisionApp from './components/IndecisionApp'
 
+const Layout = (props) => {
+    return(
+        <div>
+        <p>Header</p>
+        {props.children}
+        <p>Footer</p>
+        </div>
+    )
+}
 
-ReactDOM.render(<IndecisionApp/>, document.getElementById("app"))
+ReactDOM.render(
+    <Layout>
+        <div>
+            <h1>Page Title</h1>
+            <p>This is my page</p>
+        </div>
+    </Layout>, 
+    document.getElementById("app"))
+// ReactDOM.render(<IndecisionApp/>, document.getElementById("app"))
 
 // const template = <p>This JSX webpack</p>
 // ReactDOM.render(template, document.getElementById('app'))
