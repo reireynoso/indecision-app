6c9587b29554fefa9dd6a5a9f0158c76e6ff4881
reireynoso <reireynoso@gmail.com>
2020-03-19 20:08:55 -0400

    refactored functions for implicit return

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-const Option = (props) => {
-    return (
-        <div>
-            {props.option}
-            <button onClick={(e) => props.handleDeleteSpecificOption(props.option)}>Remove</button>
-        </div>
-    )
-}
+const Option = (props) => (
+    <div>
+        {props.option}
+        <button onClick={(e) => props.handleDeleteSpecificOption(props.option)}>Remove</button>
+    </div>
+)
+
 
 export default Option
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import Option from './Option'
-const Options = (props) => {
-    return (
-        <div>
-            <button onClick={props.handleDeleteOptions}>Remove All Button</button>
-            {props.options.length === 0 && <p>Add some options</p>}
-            {
-                props.options.map(option =>  
-                <Option 
-                    key={option} 
-                    handleDeleteSpecificOption={props.handleDeleteSpecificOption} 
-                    option={option} 
-                />)
-            }
-        </div>
-    )
-}
+const Options = (props) => (
+    <div>
+        <button onClick={props.handleDeleteOptions}>Remove All Button</button>
+        {props.options.length === 0 && <p>Add some options</p>}
+        {
+            props.options.map(option =>  
+            <Option 
+                key={option} 
+                handleDeleteSpecificOption={props.handleDeleteSpecificOption} 
+                option={option} 
+            />)
+        }
+    </div>
+)
+
 
 export default Options
