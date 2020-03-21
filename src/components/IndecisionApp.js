53ffef611dada06678fca3af51fc10068429634f
reireynoso <reireynoso@gmail.com>
2020-03-20 20:26:20 -0400

    theming variables

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -110,18 +110,20 @@ export default class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header subtitle={subtitle} />
-                <Action 
-                    hasOptions={ this.state.options.length > 0}
-                    handlePick={this.handlePick}
-                />
-                <Options 
-                    options={this.state.options}
-                    handleDeleteOptions={this.handleDeleteOptions}
-                    handleDeleteSpecificOption={this.handleDeleteSpecificOption}
-                />
-                <AddOption 
-                    handleAddOption={this.handleAddOption} 
-                />
+                <div className="container">
+                    <Action 
+                        hasOptions={ this.state.options.length > 0}
+                        handlePick={this.handlePick}
+                    />
+                    <Options 
+                        options={this.state.options}
+                        handleDeleteOptions={this.handleDeleteOptions}
+                        handleDeleteSpecificOption={this.handleDeleteSpecificOption}
+                    />
+                    <AddOption 
+                        handleAddOption={this.handleAddOption} 
+                    />
+                </div>
                 <OptionModal 
                 handleClearOption={this.handleClearOption} 
                 selectedOption={this.state.selectedOption}/>
