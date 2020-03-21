fc258a37f793e05ac4dcdf5af9b2cf91bab5711b
reireynoso <reireynoso@gmail.com>
2020-03-21 18:46:10 -0400

    widget styled

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -115,14 +115,16 @@ export default class IndecisionApp extends React.Component {
                         hasOptions={ this.state.options.length > 0}
                         handlePick={this.handlePick}
                     />
-                    <Options 
-                        options={this.state.options}
-                        handleDeleteOptions={this.handleDeleteOptions}
-                        handleDeleteSpecificOption={this.handleDeleteSpecificOption}
-                    />
-                    <AddOption 
-                        handleAddOption={this.handleAddOption} 
-                    />
+                    <div className="widget">
+                        <Options 
+                            options={this.state.options}
+                            handleDeleteOptions={this.handleDeleteOptions}
+                            handleDeleteSpecificOption={this.handleDeleteSpecificOption}
+                        />
+                        <AddOption 
+                            handleAddOption={this.handleAddOption} 
+                        />
+                    </div>
                 </div>
                 <OptionModal 
                 handleClearOption={this.handleClearOption} 
--- src/components/Options.js
+++ src/components/Options.js
@@ -2,18 +2,24 @@ import React from 'react'
 import Option from './Option'
 const Options = (props) => (
     <div>
-        <button 
-        className="button button--link"
-        onClick={props.handleDeleteOptions}>Remove All Button</button>
-        {props.options.length === 0 && <p>Add some options</p>}
-        {
-            props.options.map(option =>  
-            <Option 
-                key={option} 
-                handleDeleteSpecificOption={props.handleDeleteSpecificOption} 
-                option={option} 
-            />)
-        }
+        <div className="widget-header">
+            <h3 className="widget-header__title">Your Options</h3>
+            <button 
+            className="button button--link"
+            onClick={props.handleDeleteOptions}
+            >
+            Remove All Button
+            </button>
+            {
+                props.options.map(option =>  
+                    <Option 
+                    key={option} 
+                    handleDeleteSpecificOption={props.handleDeleteSpecificOption} 
+                    option={option} 
+                    />)
+                }
+            </div>    
+        {props.options.length === 0 && <p className="widget-p__message">Add some options</p>}
     </div>
 )
 
